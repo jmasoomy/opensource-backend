c92dd07ddbfe026a331982634af0a4d840b3bac6
jasim <masoomy05@gmail.com>
2026-04-23 19:17:25 -0400

    Set up Express Server with Initial Routes

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Backend Api is running"
+    });
+});
+
+app.get("/health", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "ok"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:3000');
+});
