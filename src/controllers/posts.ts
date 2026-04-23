08a3804e77ec58660ca3e661eeb939f2595afd3b
jasim <masoomy05@gmail.com>
2026-04-23 19:59:18 -0400

    Add get by id and create post endpoints

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,17 +1,63 @@
 import {Request, Response} from "express";
 
+// Post type
+type Post = {
+    id: number;
+    title: string;
+    content: string;
+};
+
+// List for posts
+const posts: Post[] = [
+    {
+        id: 1,
+        title: "First post",
+        content: "Some forum post information"
+    },
+    {
+        id: 2,
+        title: "Second post",
+        content: "Some forum post information"
+    }
+];
+
+
 export const getPosts = (req: Request, res: Response) => {
-    res.status(200).json([
-        {
-            id: 1,
-            title: "First post",
-            content: "Some forum post information"
-        },
-        {
-            id: 2,
-            title: "Second post",
-            content: "Some forum post information"
-        }
-    ]);
+    return res.status(200).json(posts);
+};
+
+export const getPostById = (req: Request, res: Response) => {
+    const postId = Number(req.params.id);
+    const post = posts.find((p) => p.id === postId);
+
+    // No Post Handler
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found"
+        });
+    }
+
+    return res.status(200).json(post);
+};
+
+export const createPost = (req: Request, res: Response) => {
+    const {title, content} = req.body;
+
+    // No title or content handler
+    if (!title || !content) {
+        return res.status(400).json({
+            message: "Title and content required"
+        })
+    }
+
+    const newPost: Post = {
+        id: posts.length + 1,
+        title,
+        content
+    };
+
+    posts.push(newPost);
+
+    return res.status(201).json(newPost);
 };
 
--- src/ports/rest/routes/posts.ts
+++ src/ports/rest/routes/posts.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import {getPosts} from "../../../controllers/posts";
+import {getPosts, getPostById, createPost} from "../../../controllers/posts";
 
 const router = Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPostById);
+router.post('/', createPost);
 
 export default router;
