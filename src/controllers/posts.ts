8544b20b1f3dd22f5338163d4093e1cbb3c56fd4
jasim <masoomy05@gmail.com>
2026-04-23 20:19:39 -0400

    Add update and delete posts endpoints

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -21,11 +21,12 @@ const posts: Post[] = [
     }
 ];
 
-
+// Get All Posts
 export const getPosts = (req: Request, res: Response) => {
     return res.status(200).json(posts);
 };
 
+// Get Post By Id
 export const getPostById = (req: Request, res: Response) => {
     const postId = Number(req.params.id);
     const post = posts.find((p) => p.id === postId);
@@ -40,6 +41,7 @@ export const getPostById = (req: Request, res: Response) => {
     return res.status(200).json(post);
 };
 
+// Create Post
 export const createPost = (req: Request, res: Response) => {
     const {title, content} = req.body;
 
@@ -61,3 +63,47 @@ export const createPost = (req: Request, res: Response) => {
     return res.status(201).json(newPost);
 };
 
+// Update Post
+export const updatePost = (req: Request, res: Response) => {
+    const postId = Number(req.params.id);
+    const {title, content} = req.body;
+    const post = posts.find((p) => p.id === postId);
+
+    // No Post Handler
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found"
+        });
+    }
+
+    if (!title || !content) {
+        return res.status(400).json({
+            message: "Title and content are required"
+        });
+    }
+
+    post.title = title;
+    post.content = content;
+    
+    return res.status(200).json(post);
+};
+
+// Delete Post
+export const deletePost = (req: Request, res: Response) => {
+    const postId = Number(req.params.id);
+    const postIndex = posts.findIndex((p) => p.id === postId);
+
+    // No post found handler
+    if (postIndex === -1) {
+        return res.status(404).json({
+            message: "Post not found"
+        });
+    }
+
+    const deletedPost = posts.splice(postIndex, 1)[0];
+    
+    return res.status(200).json({
+        message: "Post deleted",
+        post: deletedPost
+    });
+};
--- src/ports/rest/routes/posts.ts
+++ src/ports/rest/routes/posts.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
-import {getPosts, getPostById, createPost} from "../../../controllers/posts";
+import {getPosts, getPostById, createPost, updatePost, deletePost} from "../../../controllers/posts";
 
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
 router.post('/', createPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
