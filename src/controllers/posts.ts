0745206959126e230433e1588a08a8a62e0a4a83
jasim <masoomy05@gmail.com>
2026-04-23 23:23:21 -0400

    Added authorization for post updating and deleting - user and super

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -5,6 +5,8 @@ type Post = {
     id: number;
     title: string;
     content: string;
+    // Adding userid to post type for auth
+    userId: number;
 };
 
 // List for posts
@@ -12,12 +14,14 @@ const posts: Post[] = [
     {
         id: 1,
         title: "First post",
-        content: "Some forum post information"
+        content: "Some forum post information",
+        userId: 1
     },
     {
         id: 2,
         title: "Second post",
-        content: "Some forum post information"
+        content: "Some forum post information",
+        userId: 2
     }
 ];
 
@@ -44,6 +48,7 @@ export const getPostById = (req: Request, res: Response) => {
 // Create Post
 export const createPost = (req: Request, res: Response) => {
     const {title, content} = req.body;
+    const user = (req as any).user;
 
     // No title or content handler
     if (!title || !content) {
@@ -55,7 +60,8 @@ export const createPost = (req: Request, res: Response) => {
     const newPost: Post = {
         id: posts.length + 1,
         title,
-        content
+        content,
+        userId: user.id
     };
 
     posts.push(newPost);
@@ -68,6 +74,9 @@ export const updatePost = (req: Request, res: Response) => {
     const postId = Number(req.params.id);
     const {title, content} = req.body;
     const post = posts.find((p) => p.id === postId);
+    // Getting user for authentication
+    const user = (req as any).user;
+    
 
     // No Post Handler
     if (!post) {
@@ -82,6 +91,18 @@ export const updatePost = (req: Request, res: Response) => {
         });
     }
 
+    // Checking is post user has same id as user trying to edit
+    const isOwner = post.userId === user.id;
+    // If user is super they can update any post
+    const isSuperUser = user.role === "super"
+
+
+    if (!isOwner && !isSuperUser) {
+        return res.status(403).json({
+            message: "Not authorized to update"
+        });
+    }
+
     post.title = title;
     post.content = content;
     
@@ -92,12 +113,26 @@ export const updatePost = (req: Request, res: Response) => {
 export const deletePost = (req: Request, res: Response) => {
     const postId = Number(req.params.id);
     const postIndex = posts.findIndex((p) => p.id === postId);
+    // Getting user for auth
+    const user = (req as any).user
 
     // No post found handler
     if (postIndex === -1) {
         return res.status(404).json({
             message: "Post not found"
         });
+    }
+
+    const post = posts[postIndex];
+    // Checking if post user has same id as user trying to delete
+    const isOwner = post.userId === user.id
+    // If user is super they can delete any post
+    const isSuperUser = user.role === "super";
+
+    if (!isOwner && !isSuperUser) {
+        return res.status(403).json({
+            message: "Not authorized to delete"
+        })
     }
 
     const deletedPost = posts.splice(postIndex, 1)[0];
--- src/ports/rest/routes/posts.ts
+++ src/ports/rest/routes/posts.ts
@@ -1,12 +1,13 @@
 import {Router} from "express";
 import {getPosts, getPostById, createPost, updatePost, deletePost} from "../../../controllers/posts";
+import {verifyToken} from "../middleware/auth";
 
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
-router.post('/', createPost);
-router.put('/:id', updatePost);
-router.delete('/:id', deletePost);
+router.post('/', verifyToken, createPost);
+router.put('/:id', verifyToken, updatePost);
+router.delete('/:id', verifyToken, deletePost);
 
 export default router;
